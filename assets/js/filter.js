9c3867ff8951b62950518d8ca448b663acf8da95
startup58 <marketingzone58@gmail.com>
2023-07-28 21:39:44 +0530

    /

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -8,25 +8,62 @@ const boxesData = [
         tagline: "Embrace the charm of countryside living with our exquisite form houses.",
     },
     {
-        image: "/assets/img/popular1.jpg",
-        title: " Chettinad Farm House",
-        location: "Chinnathagadam, Coimbatore",
+        image: "/assets/img/popular2.jpg",
+        title: "Doctor Colony",
+        location: "Palladam, Coimbatore",
         buildingType:"Building Type 1",
-        tagline: "Embrace the charm of countryside living with our exquisite form houses.",
+        tagline: "Discover your perfect haven at Doctor Colony. Experience the serenity of countryside living with modern amenities at your doorstep.",
+    }, {
+        image: "/assets/img/popular3.jpg",
+        title: " Kerala Themed Farm House",
+        location: "Navakkarai, Coimbatore",
+        buildingType:"Building Type 1",
+        tagline: "Indulge in the serene ambiance of our Kerala Themed Farm House at Navakkarai, Coimbatore, and experience luxury amidst nature.",
+    }, {
+        image: "/assets/img/popular4.jpg",
+        title: "Krishna Avenue",
+        location: "Dhayanur, Karamadai",
+        buildingType:"Building Type 1",
+        tagline: "Unlock Your Dream Home: Explore Plots and Homes in Prime Locations.",
+    }, 
+    {
+        image: "/assets/img/popular5.jpg",
+        title: " Sri Nila Avenue",
+        location: "Ammaplayam, Tiruppur",
+        buildingType:"Building Type 1",
+        tagline: "Experience the serenity of Sri Nila Avenue. Enjoy a tranquil and peaceful lifestyle amidst lush greenery and serene surroundings.",
     }, {
         image: "/assets/img/popular1.jpg",
-        title: " Chettinad Farm House",
-        location: "Chinnathagadam, Coimbatore",
+        title: " Blissful Hill",
+        location: "Anaikatti, Sholayur",
         buildingType:"Building Type 1",
-        tagline: "Embrace the charm of countryside living with our exquisite form houses.",
+        tagline: "Indulge in the blissful countryside living experience at Blissful Hill's Premium Farmlands.Embrace a life of pure serenity and rejuvenation at Blissful Hill.",
     }, {
-        image: "/assets/img/popular1.jpg",
-        title: " Chettinad Farm House",
-        location: "Chinnathagadam, Coimbatore",
+        image: "/assets/img/popular2.jpg",
+        title: " RJ Garden",
+        location: "Karamadai",
+        buildingType:"Building Type 1",
+        tagline: " Your Vision, Our Expertise: Discover Plots and Homes with Endless Potential.",
+    }, {
+        image: "/assets/img/popular3.jpg",
+        title: "  Safari Farms ",
+        location: "Velliyankadu, Karamadai",
         buildingType:"Building Type 1",
         tagline: "Embrace the charm of countryside living with our exquisite form houses.",
     }, 
-   
+    {
+        image: "/assets/img/popular4.jpg",
+        title: " Lushberg Luxury Villas",
+        location: "Kotagiri, Towards Ooty",
+        buildingType:"Building Type 1",
+        tagline: " Experience luxury and comfort in our modern and elegant villas.",
+    }, {
+        image: "/assets/img/popular5.jpg",
+        title: "Legacy Estate",
+        location: "Nanjundapuram, Coimbatore",
+        buildingType:"Building Type 1",
+        tagline: "Invest in Your Tomorrow: Plots and Homes for the Savvy Real Estate Investor.",
+    }, 
    
     // Add more box data objects as needed
 ];
